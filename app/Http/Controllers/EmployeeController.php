dbece7025fd8ecfabf044284ee560ac2dd904aae
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-03-13 22:57:24 +0900

    list定義

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -35,4 +35,20 @@ class EmployeeController extends Controller
          
         return $response;
       }
+
+      public function list(){
+
+        try {
+  
+          $data = Employee::with("role")->get();
+          $response['data'] = $data;
+          $response['success'] = true;
+  
+        } catch (\Exception $e) {
+          $response['message'] = $e->getMessage();
+          $response['success'] = false;
+        }
+        return $response;
+  
+      }
 }
