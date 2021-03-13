bde7d4966968e01eebd64d22aead57f7bf718922
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-03-13 22:27:04 +0900

    EmployeeController create

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/employee', 'App\Http\Controllers\EmployeeController@index');
+Route::get('/employee/index', 'App\Http\Controllers\EmployeeController@index');
+Route::get('/employee/list', 'App\Http\Controllers\EmployeeController@index');
+Route::get('/employee/form', 'App\Http\Controllers\EmployeeController@index');
+Route::get('/employee/edit/{num}', 'App\Http\Controllers\EmployeeController@index');
+
