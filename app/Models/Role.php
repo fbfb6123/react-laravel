912f28b527d22a7477df3a88bdc999cb89fa45c8
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-03-13 22:44:00 +0900

    Update Role.php

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+
+    protected $table = "role";
+
+    protected  $primaryKey = "rol_id";
+
+    protected $fillable = [
+      'rol_name'
+    ];
+
+    public $timestamps = false;
 }
