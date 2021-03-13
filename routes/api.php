3b3afdd736c6afabfdfc7f3aee8b8dfe09a4ce9d
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-03-13 22:55:08 +0900

    Employee.js create

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/employee/role', 'App\Http\Controllers\API\EmployeeController@list_role');
+
+Route::post('/employee/create', 'App\Http\Controllers\API\EmployeeController@create');
