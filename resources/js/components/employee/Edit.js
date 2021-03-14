8ca25f841997330144534b827fd3c7c5d3809ba4
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-03-14 23:18:48 +0900

    Update Edit.js

--- resources/js/components/employee/Edit.js
+++ resources/js/components/employee/Edit.js
@@ -5,7 +5,7 @@ import employeeServices from "../../services/Employee"
 function Edit(props){
 
   const [ id, setId ] = useState(null);
-  const [ name, setName ] = useState("ppp");
+  const [ name, setName ] = useState("");
   const [ email, setEmail ] = useState(null);
   const [ city, setCity ] = useState(null);
   const [ address, setAddress ] = useState(null);
@@ -44,7 +44,25 @@ function Edit(props){
     }
     fetchDataRol();
 
-  },[])
+  },[]);
+
+
+  const updateEmployee = async () => {
+
+    const data = {
+      id, name, email, city, address, phone, rol
+    }
+
+    const res = await employeeServices.update(data);
+
+    if (res.success) {
+      alert(res.message)
+    }
+    else {
+      alert(res.message)
+    }
+
+  }
 
 
   return (
@@ -54,28 +72,24 @@ function Edit(props){
       <div class="row">
         <div class="col-md-6 mb-3">
           <label for="firstName">Name</label>
-          <input type="text" class="form-control" value={name} />
+          <input type="text" class="form-control" value={name}
+          onChange={(event)=>setName(event.target.value)} />
         </div>
       </div>
 
       <div class="row">
         <div class="col-md-6 mb-3">
           <label for="email">Email</label>
-          <input type="email" class="form-control" placeholder="you@example.com"  value={email}/>
+          <input type="email" class="form-control" placeholder="you@example.com"
+          value={email} onChange={(event)=>setEmail(event.target.value)}/>
         </div>
       </div>
 
       <div class="row">
         <div class="col-md-6 mb-3">
-          <label for="address">Address</label>
-          <input type="text" class="form-control" placeholder="1234 Main St"  value={address}/>
-        </div>
-      </div>
-      
-      <div class="row">
-        <div class="col-md-6 mb-3">
           <label for="phone">City {city}</label>
-          <select id="inputState" class="form-control" value={city} >
+          <select id="inputState" class="form-control" value={city}
+          onChange={(event)=>setCity(event.target.value)} >
              <option selected>Choose...</option>
              <option value="New York">New York</option>
              <option value="London">London</option>
@@ -86,15 +100,25 @@ function Edit(props){
 
       <div class="row">
         <div class="col-md-6 mb-3">
+          <label for="address">Address</label>
+          <input type="text" class="form-control" placeholder="1234 Main St"
+          value={address} onChange={(event)=>setAddress(event.target.value)}/>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-6 mb-3">
           <label for="address">Phone </label>
-          <input type="text" class="form-control" placeholder="123467890"  value={phone} />
+          <input type="text" class="form-control" placeholder="123467890"  value={phone}
+          onChange={(event)=>setPhone(event.target.value)}/>
         </div>
       </div>
 
       <div class="row">
         <div class="col-md-6 mb-3">
           <label for="phone">Rol</label>
-          <select id="inputState" class="form-control" value={rol}>
+          <select id="inputState" class="form-control" value={rol}
+          onChange={(event)=>setRol(event.target.value)}>
              {
                listRol.map((itemselect)=>{
                  return(
@@ -108,7 +132,8 @@ function Edit(props){
 
       <div class="row">
         <div class="col-md-6 mb-3">
-          <button class="btn btn-primary btn-block" type="submit">Save</button>
+          <button onClick={()=>updateEmployee()}
+          class="btn btn-primary btn-block" type="submit">Save</button>
         </div>
       </div>
     </div>
